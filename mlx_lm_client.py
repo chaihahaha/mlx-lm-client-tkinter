0c4e4ebcfb44f1cda1e372846fbac811ca6b77f6
chaihahaha <chai836275709@gmail.com>
2025-03-01 18:56:10 +0800

    add asserts

--- mlx_lm_client.py
+++ mlx_lm_client.py
@@ -39,10 +39,64 @@ args = parser.parse_args()
 # Load configuration from file
 config = configparser.ConfigParser(interpolation=FileInterpolation())
 config.read(args.config_file)
+instruct_config = dict(config["INSTRUCT"])
+
+# Assert config fields
+assert "system_prefix" in instruct_config.keys()
+assert "system_suffix" in instruct_config.keys()
+assert "input_prefix" in instruct_config.keys()
+assert "input_suffix" in instruct_config.keys()
+assert "output_prefix" in instruct_config.keys()
+assert "output_suffix" in instruct_config.keys()
+assert "char" in instruct_config.keys()
+assert "user" in instruct_config.keys()
+assert "system_sequence" in instruct_config.keys()
+assert "stop_sequence" in instruct_config.keys()
+assert "last_output_prefix" in instruct_config.keys()
+assert "global_chat_history" in instruct_config.keys()
+assert "server_address" in instruct_config.keys()
 
 # Update locals() with the config dictionary
-locals().update(dict(config['INSTRUCT']))
+locals().update(instruct_config)
 autoscroll = False
+
+params = dict()
+with open(args.param_file,'r') as f:
+    params.update(yaml.safe_load(f))
+
+assert "add_bos_token" in params.keys()
+assert "do_sample" in params.keys()
+assert "dynatemp_base" in params.keys()
+assert "early_stopping" in params.keys()
+assert "epsilon_cutoff" in params.keys()
+assert "eta_cutoff" in params.keys()
+assert "grammar" in params.keys()
+assert "guidance_scale" in params.keys()
+assert "length_penalty" in params.keys()
+assert "max_context_length" in params.keys()
+assert "max_tokens" in params.keys()
+assert "min_length" in params.keys()
+assert "min_p" in params.keys()
+assert "mirostat" in params.keys()
+assert "mirostat_eta" in params.keys()
+assert "mirostat_tau" in params.keys()
+assert "negative_prompt" in params.keys()
+assert "no_repeat_ngram_size" in params.keys()
+assert "num_beams" in params.keys()
+assert "penalty_alpha" in params.keys()
+assert "repetition_penalty" in params.keys()
+assert "repetition_penalty_range" in params.keys()
+assert "seed" in params.keys()
+assert "skip_special_tokens" in params.keys()
+assert "smoothing_factor" in params.keys()
+assert "stream" in params.keys()
+assert "temperature" in params.keys()
+assert "tfs" in params.keys()
+assert "top_a" in params.keys()
+assert "top_k" in params.keys()
+assert "top_p" in params.keys()
+assert "typical_p" in params.keys()
+assert "use_default_badwordids" in params.keys()
 
 # Function to send the message and display it in the GUI
 def send_message(user_text_area):
@@ -70,8 +124,7 @@ def send_request(user_input):
         "prompt": global_chat_history,
         "stop_sequence": [stop_sequence],
     }
-    with open(args.param_file,'r') as f:
-        payload.update(yaml.safe_load(f))
+    payload.update(params)
 
     try:
         response = requests.post(server_address, json=payload, stream=True)
